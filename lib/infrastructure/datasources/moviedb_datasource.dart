e57800354ee05a0d12a98c5f8a5ffa9867f7fad7
Lucas <lucasperez.ad47@gmail.com>
2025-01-22 19:07:46 -0300

    search delegate

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -77,4 +77,13 @@ class MovieDbDatasource extends MoviesDatasource {
 
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response = await dio.get(
+      '/search/movie',
+      queryParameters: {'query': query},
+    );
+    return _jsontoMovies(response.data);
+  }
 }
---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SearchMovieDelegate extends SearchDelegate {
+  @override
+  String get searchFieldLabel => 'Buscar Pelicula';
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      if (query.isNotEmpty)
+        IconButton(onPressed: () => query = '', icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_ios_new_rounded));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Text('buildSuggestions');
+  }
+}
